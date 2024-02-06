926b05009cfc473a5112cd3646621e80e7ca1139
Abdo Belhaje <abdelkodoussebelhaje@gmail.com>
2024-02-06 23:44:23 +0100

    add files

---
+++ springtest/src/main/java/com/example/springtest/config/webConfig.java
@@ -0,0 +1,24 @@
+package com.example.springtest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webConfig {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+
+}
